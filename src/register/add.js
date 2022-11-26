575dfcb703b929dda7841345e7f521583e1a004c
Makhuta <matykubla@gmail.com>
2022-11-26 02:41:39 +0100

    Fix
    Fixed permission error

--- src/register/add.js
+++ src/register/add.js
@@ -2,12 +2,9 @@ module.exports = async function ({ cmds, NRCMD }) {
   if (NRCMD.HasCreate) {
     await require(NRCMD.Path)?.create({
       commands: cmds,
-      permissions: (function () {
-        if (NRCMD.IsOwnerDependent || NRCMD.IsAdminDependent) {
-          if (NRCMD.RequiedUserPermissions.length < 1) return "0";
-          else return require("../other/PermissionsList.json")[NRCMD.RequiedUserPermissions];
-        }
-      })(),
+      permissions: require(join(Configs, "PermissionsList.json"))[this.RequiedUserPermissions]
+        ? require(join(Configs, "PermissionsList.json"))[this.RequiedUserPermissions]
+        : "0",
       dmEnabled: NRCMD.PMEnable
     });
   } else {
@@ -15,12 +12,9 @@ module.exports = async function ({ cmds, NRCMD }) {
       name: NRCMD.Name.toLowerCase(),
       description: NRCMD.DescriptionShort,
       dmPermission: NRCMD.PMEnable,
-      defaultMemberPermissions: (function () {
-        if (NRCMD.IsOwnerDependent || NRCMD.IsAdminDependent) {
-          if (NRCMD.RequiedUserPermissions.length < 1) return "0";
-          else return require("../other/PermissionsList.json")[NRCMD.RequiedUserPermissions];
-        }
-      })()
+      defaultMemberPermissions: require(join(Configs, "PermissionsList.json"))[this.RequiedUserPermissions]
+        ? require(join(Configs, "PermissionsList.json"))[this.RequiedUserPermissions]
+        : "0"
     });
   }
 };
